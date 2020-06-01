c505da60837fc6d7e3dbf50f0e89933eb7685c53
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-06-01 15:53:44 +0530

    Base level setup,  ball,paddle and level loader scripts written

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    //[SerializeField] float levelLoadDelay = 2f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartDeathSequence();
+    }
+
+    private static void StartDeathSequence()
+    {
+        Debug.Log("Death");
+        SceneManager.LoadScene("Game Over");
+    }
+}
