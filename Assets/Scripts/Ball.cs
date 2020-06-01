0547cf19b491742925f875e786465e41c351e54e
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-06-01 23:36:03 +0530

    Ball Launch , gravity settings Updated

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     [SerializeField] Paddle paddle1 = null;
-
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    //State
+    bool hasStarted = false;
     Vector2 paddleToBallVector;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
     }
 }
