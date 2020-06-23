5e30269792155f06d2156da255831640ba9d77e8
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-06-23 22:41:17 +0530

    Bug Fixes, Ball movement stopped

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     [SerializeField] int breakableBlocks = 0;
-
+    Ball ball;
     SceneLoader sceneLoader;
 
     private void Start()
     {
+        ball = FindObjectOfType<Ball>();
         sceneLoader = FindObjectOfType<SceneLoader>();
     }
     public void CountBlocks()
@@ -22,6 +23,7 @@ public class Level : MonoBehaviour
         breakableBlocks--;
         if(breakableBlocks <= 0)
         {
+            ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
             sceneLoader.LoadNextScene();
         }
     }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,6 +7,7 @@ public class SceneLoader : MonoBehaviour {
 
     public Animator transition;
     [SerializeField] float transitionTime = 1f;
+    
 
     public void LoadNextScene()
     {
